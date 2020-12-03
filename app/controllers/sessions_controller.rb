1441c16e2ea63d01187b595b434808e86dfb2f76
Trini <trini.4595@gmail.com>
2020-12-03 16:06:47 -0300

    first commmit

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by_email(params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to root_path, notice; 'Logueado'
+    else
+      flash.now[:notice] = 'Email o password inválidos'
+      render action: :new
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_url, notice: 'Sign out successfully'
+  end
+end
